a6194fcb6b2298e4e31275dddbdf1e831a5298ec
zoown12 <zoown12@gmail.com>
2024-10-22 10:23:31 +0900

    Devops_T

---
+++ src/main/java/com/example/devops/DevopsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTestApplication.class, args);
+		System.out.println("SERVER START....");
+	}
+
+}
